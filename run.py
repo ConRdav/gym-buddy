ab954054eab0079bea70833639388e20f9c3e775
ConRdav <connor@redwoodglade.com>
2022-01-25 21:58:06 +0000

    Add get_workout_routine and show_routine function

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,16 +13,33 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('gym-buddy')
 
-three = SHEET.worksheet('three')
-four = SHEET.worksheet('four')
-five = SHEET.worksheet('five')
+three_day = SHEET.worksheet('three')
+four_day = SHEET.worksheet('four')
+five_day = SHEET.worksheet('five')
 workouts = SHEET.worksheet('workouts')
 
-data1 = three.get_all_values()
-data2 = four.get_all_values()
-data3 = five.get_all_values()
-data4 = workouts.get_all_values()
-print(data1)
-print(data2)
-print(data3)
-print(data4)
+
+def get_workout_routine():
+    """
+    Gets the workout routine requested by the user
+    """
+    print("Please choose a workout routine.")
+    print("Your options are a 3 day, 4 day or 5 day routine.")
+    print("Please choose your preferred routine by entering 3, 4 or 5.\n")
+    routine = input("Enter your choice here: ")
+    show_routine(routine)
+
+
+def show_routine(routine):
+    """
+    prints user requested routine
+    """
+    if routine == '3':
+        pprint(three_day.get_all_values())
+    elif routine == '4':
+        pprint(four_day.get_all_values())
+    else:
+        pprint(five_day.get_all_values())
+
+
+get_workout_routine()
