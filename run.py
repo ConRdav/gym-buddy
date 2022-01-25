59936cf35caeee73ede1b184b773d1a2c5afa3d1
ConRdav <connor@redwoodglade.com>
2022-01-25 22:01:07 +0000

    Add validate_routine function and import re

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import re
 from pprint import pprint
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -23,11 +25,27 @@ def get_workout_routine():
     """
     Gets the workout routine requested by the user
     """
-    print("Please choose a workout routine.")
-    print("Your options are a 3 day, 4 day or 5 day routine.")
-    print("Please choose your preferred routine by entering 3, 4 or 5.\n")
-    routine = input("Enter your choice here: ")
+    while True:
+        print("Please choose a workout routine.")
+        print("Your options are a 3 day, 4 day or 5 day routine.")
+        print("Please choose your preferred routine by entering 3, 4 or 5.\n")
+
+        routine = input("Enter your choice here: ")
+        if validate_routine(routine):
+            print(f"You picked the {routine} day workout routine.")
+            break
+        else:
+            print("Invalid entry, please enter 3, 4 or 5.")
+
     show_routine(routine)
+
+
+def validate_routine(routine):
+    """
+    validates the user input
+    """
+    pattern = re.compile('[3-5]{1}')
+    return pattern.match(routine)
 
 
 def show_routine(routine):
