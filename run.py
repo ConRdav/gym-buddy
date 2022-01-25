66c8e1e87d478a2b3f25b21c9dcdebc6d12c2483
ConRdav <connor@redwoodglade.com>
2022-01-25 21:45:57 +0000

    Wire up APIs and confirm access to google sheet

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('gym-buddy')
+
+three = SHEET.worksheet('three')
+four = SHEET.worksheet('four')
+five = SHEET.worksheet('five')
+workouts = SHEET.worksheet('workouts')
+
+data1 = three.get_all_values()
+data2 = four.get_all_values()
+data3 = five.get_all_values()
+data4 = workouts.get_all_values()
+print(data1)
+print(data2)
+print(data3)
+print(data4)
